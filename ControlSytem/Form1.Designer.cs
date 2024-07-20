2136d232b896c1a11f070340f361b57c76670cc0
aymane el yamani <aymaneelyamani3@gmail.com>
2024-07-20 02:04:44 +0100

    add linear and discrete models

--- ControlSytem/Form1.Designer.cs
+++ ControlSytem/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(144, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(510, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Output";
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(109, 144);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(125, 27);
+            this.Input.TabIndex = 2;
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(481, 144);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(125, 27);
+            this.Output.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(495, 345);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox Input;
+        private TextBox Output;
+        private Button button1;
     }
 }
--- ControlSytem/Form1.cs
+++ ControlSytem/Form1.cs
@@ -2,9 +2,42 @@ namespace ControlSytem
 {
     public partial class Form1 : Form
     {
+        double Ts = 0.1;
+        double xk = 0;
+        double u = 1;
         public Form1()
         {
             InitializeComponent();
+            Input.Text = u.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+
+            u = Convert.ToDouble(Input.Text);
+            xk = DiscreteModel(xk, u);
+            Output.Text = xk.ToString();
+        }
+        double LinearModel(double x)
+        {
+            double a = 2;
+            double b = 4;
+            double y;
+
+            y = a * x + b;
+
+            return y;
+        }
+        double DiscreteModel(double xk, double u)
+        {
+            double a = 0.25;
+            double b = 2;
+            double xk1;
+
+            xk1 = (1 - Ts * a) * xk + Ts * b * u;
+
+            return xk1;
         }
     }
 }
